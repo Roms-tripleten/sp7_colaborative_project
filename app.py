a22d801e6e6e429ac302bf73bdbe453e2318bc8e
mavegam <mavegamontoya@gmail.com>
2025-07-31 09:55:45 +0800

    Ejercicio de la clase

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import matplotlib.pyplot as plt
 
 st.set_page_config(page_title='Mi Dashboard')
 st.header('📊 Mi dashboard')
+movies = pd.read_csv(r'C:\Users\mayrmon\Documents\TripleTen\Sprint_7_Clase_Webinar\sp7_colaborative_project\dataset\imdb_movies.csv')
+st.subheader('Películas de IMDB')
+st.dataframe(movies)
+st.subheader('Gráfico de Películas por Año')
+fig = px.histogram(movies, x='Released_Year', title='Número de Películas por Año')
+st.plotly_chart(fig, use_container_width=True)
+show_df = st.checkbox('Mostrar tabla de películas', value=True)
+if show_df:
+    st.subheader('Películas de IMDB')
+    st.dataframe(movies)
+fig, ax = plt.subplots()
+movies['Gross']=pd.to_numeric(movies['Gross'].str.replace(',',''),errors='coerce')
+movies['Gross'].plot(kind='box', title='Cuartiles de Gross', grid=True)
+st.pyplot(fig)
+fig = px.box(movies, x='Certificate', y='Gross', title='Boxplot de "Gross" por categoría (Certificate)')
+fig.update_layout(
+    xaxis_title='Categoría',
+    yaxis_title='Valor',
+    boxmode='group'  # Por si tienes múltiples trazas en el futuro
+)
+st.plotly_chart(fig)
+
--- calculadora.py
+++ calculadora.py
@@ -21,21 +21,59 @@ class Calculator:
         a, b = numbers
         if operation == "add":
             self.add(a, b)
+        elif operation == "subtract":
+            self.substract(a, b)
+        elif operation == "multiply":
+            self.multiply(a, b)
+        elif operation == "divide":
+            self.divide(a, b)    
         else:
             print(f"🚧 Operation '{operation}' not implemented yet.")
 
-    #def substract(self,a,b)
-    #def multiply(self,a,b)
-    #def divide(self,a,b)
+    def substract(self,a,b):
+        result = a - b
+        if self.verbose:
+            print(f"🧮 Subtracting {a} - {b} = {result}")
+        else:
+            print(result)
+        self.memory = result    
+    
+    def multiply(self,a,b):
+        result = a * b
+        if self.verbose:
+            print(f"🧮 Multiplying {a} * {b} = {result}")
+        else:
+            print(result)
+        self.memory = result
+
+    def divide(self,a,b):
+        if b == 0:
+            print("🚫 Error: Division by zero is not allowed.")
+            return
+        result = a / b
+        if self.verbose:
+            print(f"🧮 Dividing {a} / {b} = {result}")
+        else:
+            print(result)
+        self.memory = result
     ## Opcional
-    #def show_memory(self)
+    def show_memory(self):
+        if self.memory:
+            print(f"🧠 Memory: {self.memory}")
+        else:
+            print("🧠 Memory is empty.")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Mini Calculator CLI")
     parser.add_argument("--verbose", action="store_true", help="Show detailed output")
     parser.add_argument("--operation", type=str, choices=["add", "subtract", "multiply", "divide"], help="Math operation to perform")
     parser.add_argument("--numbers", nargs=2, type=float, help="Two numbers to operate with")
+    parser.add_argument("--memory", action="store_true", help="Show memory")
     args = parser.parse_args()
 
     calc = Calculator(verbose=args.verbose)
-    calc.operate(operation=args.operation, numbers=args.numbers)
+    if args.memory:
+        calc.show_memory()
+    else:
+        calc.operate(operation=args.operation, numbers=args.numbers)
